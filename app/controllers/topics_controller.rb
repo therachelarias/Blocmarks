c5c0edd9002ea87c9e2db97bd96a078b1793d737
Rachel <rachelmramirez@gmail.com>
2015-11-08 16:26:26 -0500

    what the CRUD

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  
   def index
     @topics = Topic.all
   end
@@ -8,8 +9,32 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def  create
+    @topic = Topic.new(params.require(:topic).permit(:title, :body))
+    if @topic.save
+      flash[:notice] = "Topic was saved."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update_attributes(param.require(:topic).permit(:title, :body))
+      flash[:notice] = "Topic was updated."
+      redirect_to @topic
+    else
+      flash[:error] = "Ther was an error saving. Please try again."
+      render :edit
+    end
   end
 end
