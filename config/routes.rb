685de78b0a952110b3116e62ae90e8e5a932143d
Rachel <rachelmramirez@gmail.com>
2015-12-07 18:06:34 -0500

    like unlike

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   resources :topics do
-    resources :bookmarks, except: [:index] do
-      resources :likes, only: [:create, :destroy]
-    end
+    resources :bookmarks, except: [:index]
+  end
+
+  resources :bookmarks, only: [ ] do
+    resources :likes, only: [:create, :destroy]    
   end
 
   devise_for :users
