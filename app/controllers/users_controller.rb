2073836af4f0921d08a631c24e85f36dbc9081e9
Rachel <rachelmramirez@gmail.com>
2015-12-12 14:14:00 -0500

    users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @user_bookmarks = @user.bookmarks
+    @liked_bookmarks = Like.where(user_id: current_user.id)
   end
 end
